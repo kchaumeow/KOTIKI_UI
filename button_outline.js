c6888c8453f00eb0b8917c563be56b3754e3ac77
kchaumeow <atrofimova516@gmail.com>
2023-03-14 10:56:15 +0300

    first commit

---
+++ button_outline.js
@@ -0,0 +1,5 @@
+const Button = () => React.createElement("button", {
+    class: "btn-outline-primary",
+    type: "submit"
+  }, "SecondaryButtonText");
+  export default Button;
---
+++ button_primary.js
@@ -0,0 +1,5 @@
+const Button = () => React.createElement("button", {
+    class: "btn-primary",
+    type: "submit"
+  }, "SecondaryButtonText");
+  export default Button;
