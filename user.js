cda9579562dba1c4ce074a793558deb81b9df0b9
kchaumeow <atrofimova516@gmail.com>
2023-03-19 11:30:18 +0300

    add: class user

---
+++ user.js
@@ -0,0 +1,24 @@
+export default class User {
+    constructor({
+      id,
+      name,
+      imageURL,
+      description,
+      email,
+      role,
+      followersCount,
+      followingCount,
+      postsCount,
+    }) {
+      this.id = id;
+      this.name = name;
+      this.imageURL = imageURL;
+      this.description = description;
+      this.email = email;
+      this.role = role;
+      this.followersCount = followersCount;
+      this.followingCount = followingCount;
+      this.postsCount = postsCount;
+    }
+  }
+  
