e658770b1f0497b2411014dfdfe664590e1603e2
kchaumeow <atrofimova516@gmail.com>
2023-03-24 10:04:40 +0300

    feat: navbar & photo-uploader

--- FeedSwitcher.jsx
+++ FeedSwitcher.jsx
@@ -1,8 +1,13 @@
-export default function FeedSwitcher(){
+export default function FeedSwitcher({ active, options, onChange }) {
     return (
-        <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
-            <input onClick={location.href = 'http://www.yandex.ru/'} type="radio" className="btn-check togglerElem" name="btnradio" id="btnradio1" autocomplete="off" />
-            <label className="btn btn-outline-primary" for="btnradio1">Radio 1</label>
-        </div>
+      <div className="btn-group" role="group" aria-label="Basic radio toggle button group" id="btnGroup">
+        {options.map((buttonName) => (
+          <label className="btn btn-outline-primary" key={buttonName}>
+            <input type="radio" className="btn-check" checked={active === buttonName} onChange={() => onChange(buttonName)} />
+            {buttonName}
+          </label>
+        ))}
+      </div>
     );
-}
+  }
+  
